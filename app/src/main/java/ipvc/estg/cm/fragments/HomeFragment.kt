4a79befb532eeb012d0c002fe9263ee6666af09b
Flávio <67983380+flavioftkode@users.noreply.github.com>
2021-06-06 15:24:11 +0100

    Home Page Fragment

--- app/src/main/java/ipvc/estg/cm/fragments/HomeFragment.kt
+++ app/src/main/java/ipvc/estg/cm/fragments/HomeFragment.kt
@@ -1,4 +1,18 @@
 package ipvc.estg.cm.fragments
 
-class HomeFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ipvc.estg.cm.R
+
+class HomeFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.cm_home_fragment, container, false)
+        return view
+    }
+
 }
