dfec737016a28a7c68ab00f2d6668c09cbefd342
Flávio <67983380+flavioftkode@users.noreply.github.com>
2021-06-06 15:05:48 +0100

    Login validações

--- app/src/main/java/ipvc/estg/cm/fragments/LoginFragment.kt
+++ app/src/main/java/ipvc/estg/cm/fragments/LoginFragment.kt
@@ -1,5 +1,7 @@
 package ipvc.estg.cm.fragments
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Bundle
@@ -12,13 +14,17 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.TextView
-import android.widget.Toolbar
 import androidx.fragment.app.Fragment
 import br.com.simplepass.loadingbutton.customViews.CircularProgressButton
 import ipvc.estg.cm.R
+import ipvc.estg.cm.entities.User
 import ipvc.estg.cm.navigation.NavigationHost
+import ipvc.estg.cm.retrofit.EndPoints
+import ipvc.estg.cm.retrofit.ServiceBuilder
 import org.json.JSONObject
-import org.w3c.dom.Text
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.util.*
 
 class LoginFragment: Fragment() {
@@ -27,7 +33,6 @@ class LoginFragment: Fragment() {
     private var btnRegister: Button? = null
     private var btnResetPassword: Button? = null
     private var rememberMe: CheckBox? = null
-    private var bntClose: Toolbar? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -42,14 +47,9 @@ class LoginFragment: Fragment() {
         btnRegister = view.findViewById(R.id.btn_register)
         btnResetPassword = view.findViewById(R.id.btn_reset_password)
         rememberMe = view.findViewById(R.id.remember_me)
-        bntClose = view.findViewById(R.id.close_login)
     }
 
     private fun setClickListeners(view: View) {
-        bntClose!!.setNavigationOnClickListener {
-            activity?.onBackPressed()
-        }
-
         btnLogin!!.setOnClickListener {
             btnLogin!!.isEnabled = false
             btnLogin!!.startAnimation()
@@ -73,14 +73,51 @@ class LoginFragment: Fragment() {
                 failed()
             }else{
                 val obj = JSONObject()
-                obj.put("username", username);
-                obj.put("password", password);
+                obj.put("username", username)
+                obj.put("password", password)
                 var payload = obj.toString()
                 payload = Base64.encodeToString(payload.toByteArray(charset("UTF-8")), Base64.DEFAULT)
 
-               /* val request = ServiceBuilder.buildService(EndPoints::class.java)
-                val call = request.loginUser(payload = payload!!)*/
+                val request = ServiceBuilder.buildService(EndPoints::class.java)
+                val call = request.loginUser(payload = payload)
+                call.enqueue(object : Callback<User> {
+                    override fun onResponse(call: Call<User>?, response: Response<User>?) {
 
+                        if (response!!.isSuccessful) {
+                            if (rememberMe!!.isChecked) {
+                                val rememberMe: SharedPreferences = context!!.getSharedPreferences("REMEMBER", Context.MODE_PRIVATE)
+                                rememberMe.edit().putString("username", "response").apply()
+                            } else {
+                                val rememberMe: SharedPreferences = context!!.getSharedPreferences("REMEMBER", Context.MODE_PRIVATE)
+                                rememberMe.edit().clear().apply()
+                            }
+                            val rememberMe: SharedPreferences = context!!.getSharedPreferences("AUTHENTICATION", Context.MODE_PRIVATE)
+                            rememberMe.edit().putInt("idUser", response.body().id).apply()
+                            rememberMe.edit().putString("_token", "Bearer " + response.body()._token).apply()
+                            success()
+                        } else {
+                            failed()
+                            if (response.code() == 403 && response.message() == "login_fail") {
+                                usernameInput.error = getString(R.string.wrong_user_info)
+                                passwordInput.error = getString(R.string.wrong_user_info)
+                            } else {
+                                (activity as NavigationHost).customToaster(title = getString(R.string.toast_error), message = getString(R.string.general_error), type = "connection")
+                            }
+                        }
+                        btnLogin!!.isEnabled = true
+                    }
+
+                    override fun onFailure(call: Call<User>?, t: Throwable?) {
+                        btnLogin!!.isEnabled = true
+
+                        val rememberMe: SharedPreferences = context!!.getSharedPreferences("AUTHENTICATION", Context.MODE_PRIVATE)
+                        rememberMe.edit().putInt("idUser", 0).apply()
+                        rememberMe.edit().putString("_token", "").apply()
+
+                        (activity as NavigationHost).customToaster(title = getString(R.string.toast_error), message = getString(R.string.general_error), type = "connection")
+                        failed()
+                    }
+                })
             }
         }
     }
